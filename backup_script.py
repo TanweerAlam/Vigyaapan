aa698b719887ccab3e12e38e8ccddecf250dd4b0
TanweerAlam <tanweeralam1312@gmail.com>
2023-02-22 12:12:57 +0000

    Backup task assigned

---
+++ backup_script.py
@@ -0,0 +1,5 @@
+cd ~;
+cd /home/testsarkari/SarkaariWebsite;
+source /home/testsarkari/.virtualenvs/sarkarivigyaapan/bin/activate;
+python manage.py dumpdata --indent 4 --exclude=contenttypes --natural-foreign --natural-primary > backup/db_backup.json;
+deactivate;
